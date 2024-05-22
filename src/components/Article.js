8f4e8fa46a1a5b4be4062218f251792c5877718b
ElvineJoy <elvinkuiyungest@gmail.com>
2024-05-22 14:34:53 +0300

    All deliverables achieved.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,28 @@
 import React from 'react'
 
-function Article({title, date="January 1, 1970", preview} ) {
-  return (
+function Article({title, date="January 1, 1970", preview, minutes} ) {
+    const cEmoji = '☕️';
+    const bEmoji = '🍱'
+
+    let emojis = "";
+    if (minutes < 30){
+        for (let i=0; i<minutes; i+=5) {
+            emojis += cEmoji
+        }
+    } else {
+        if (minutes >=30) {
+            for(let i=0; i<minutes; i+=10) {
+                emojis += bEmoji
+            }
+        }
+    }
+    return (
     <article>
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>{date}, {emojis} {minutes} min read </small>
         <p>{preview}</p>
     </article>
-  )
+  );
 }
 
 export default Article
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import blogData from '../data/blog';
 import Article from './Article';
 
-function ArticleList() {
+function ArticleList({posts}) {
     // console.log(posts);
+    
 
-    const articleList = blogData.posts.map((article) => {
-        return <Article key={article.id} title={article.title} date={article.date} preview={article.preview} />
-    })
+    const articleList = posts.map((article) => {
+        return (
+            <Article 
+                key={article.id} 
+                title={article.title} 
+                date={article.date} 
+                preview={article.preview} 
+                minutes={article.minutes}
+            />
+        ); 
+    }); 
   return (
     <main>{articleList}</main>
   )
